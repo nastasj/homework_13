c7b61b4099aed99cae8d2029088640b5c37e58cc
nastasj <a-nastja-test1@yandex.ru>
2024-06-03 23:45:28 +0300

    add system properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,10 +17,10 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "125.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "121.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        //Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
